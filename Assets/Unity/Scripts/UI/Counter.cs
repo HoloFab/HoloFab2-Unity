b1da3b2be5cddc9b4712973e867a8195eefc68ac
DKFactotum <daniil.koshelyuk@gmail.com>
2020-07-25 21:57:45 +0200

    reintroduce common unity stuff

---
+++ Assets/Unity/Scripts/UI/Counter.cs
@@ -0,0 +1,46 @@
+﻿//#define DEBUG
+#undef DEBUG
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+using HoloFab;
+using HoloFab.CustomData;
+
+namespace HoloFab {
+	// Counter element, responsible for keeping track of the value.
+	public class Counter : MonoBehaviour {
+		[Tooltip("UI elements.")]
+		public Button P, N;
+		[Tooltip("Label text to display value.")]
+		public Text label;
+		// Value of the counter.
+		[HideInInspector]
+		public int value = 0;
+        
+		// Start is called before the first frame update
+		void Start() {
+			// P.onClick.AddListener(CounterPlus);
+			// N.onClick.AddListener(CounterMinus);
+		}
+		// Update value On UI click.
+		public void CounterPlus() {
+			CounterChanged(1);
+		}
+		public void CounterMinus() {
+			CounterChanged(-1);
+		}
+		private void CounterChanged(int shift){
+			this.value += shift;
+			this.label.text = this.value.ToString();
+			#if DEBUG
+			Debug.Log("Counter: Value: " + this.value);
+			#endif
+			// Inform UI Manager.
+			ParameterUIMenu.instance.OnValueChanged();
+		}
+	}
+}
---
+++ Assets/Unity/Scripts/UI/GeneralUIMenu.cs
@@ -0,0 +1,100 @@
+﻿//#define DEBUG
+#undef DEBUG
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+#if WINDOWS_UWP
+using Microsoft.MixedReality.Toolkit;
+#elif UNITY_ANDROID
+// using GoogleARCore.Examples.Common;
+#endif
+
+using HoloFab;
+using HoloFab.CustomData;
+
+namespace HoloFab {
+	// Unity Component Controlling Addition and Removal of Generated Objects.
+	public class GeneralUIMenu : MonoBehaviour {
+		[Header("Variables Set From Scene.")]
+		[Tooltip("Button to exit application.")]
+		public Button buttonExit;
+		[Tooltip("Button to Destroy CPlane.")]
+		public Button buttonDestroyCplane;
+		[Tooltip("Button to Toggle Grid.")]
+		public Button buttonToggleGrid;
+		[Tooltip("Button to Delete Objects.")]
+		public Button buttonObjects;
+        
+		// Events to be raised on clicks:
+		// - exit application
+		public void OnExit() {
+			#if DEBUG
+			Debug.Log("General UI: exit.");
+			#endif
+			#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+			#else // UNITY_ANDROID
+			Application.Quit();
+			#endif
+		}
+		// - Destroy CPlane
+		public void OnDestroyCPlane() {
+			#if DEBUG
+			Debug.Log("General UI: reset C-Plane.");
+			#endif
+			//TODO not actually delete c plane but start placing it (put infron tf camera and activate placeable)
+			// Check for C-plane
+			if (!ObjectManager.instance.CheckCPlane()) return;
+			ObjectManager.instance.cPlane.GetComponent<Interactible_Placeable>().ForcePlacement();
+			//#if WINDOWS_UWP
+			//ObjectManager.instance.cPlane.GetComponent<Interactible_Placeable>().ForcePlacement();
+			//#else
+			//ObjectManager.instance.cPlane.SetActive(false);
+			////DestroyImmediate(ObjectManager.instance.cPlane);
+			////Resources.UnloadUnusedAssets();
+			//#endif
+		}
+		// - Toggle AR Core Grid
+		public void OnTogglePointsAndGrids() {
+			#if DEBUG
+			Debug.Log("General UI: toggle scanned environment visibility.");
+			#endif
+			ObjectManager.instance.ToggleEnvironmentMeshes();
+			#if WINDOWS_UWP
+			// Microsoft Windows MRTK
+			// Toggle Mesh Observation from all Observers
+			if (ObjectManager.instance.flagGridVisible)
+				CoreServices.SpatialAwarenessSystem.ResumeObservers();
+			else
+				CoreServices.SpatialAwarenessSystem.SuspendObservers();
+			#elif UNITY_ANDROID
+			// Android ARkit
+			DetectedPlaneVisualizerExtender.flagVisible = ObjectManager.instance.flagGridVisible;
+			#endif
+		}
+		// - Destroy Objects
+		public void OnDestroyObjects() {
+			#if DEBUG
+			Debug.Log("General UI: destroy all objects.");
+			#endif
+			ObjectManager.instance.gameObject.GetComponent<MeshProcessor>().DeleteMeshes(SourceType.TCP);
+			ObjectManager.instance.gameObject.GetComponent<MeshProcessor>().DeleteMeshes(SourceType.UDP);
+			ObjectManager.instance.gameObject.GetComponent<TagProcessor>().DeleteTags();
+			ObjectManager.instance.gameObject.GetComponent<RobotProcessor>().DeleteRobots();
+			ObjectManager.instance.gameObject.GetComponent<Point3DProcessor>().DeletePoints();
+		}
+		public void OnAdd3DPoint() {
+			#if DEBUG
+			Debug.Log("General UI: add a point.");
+			#endif
+			// Check for C-plane
+			if (!ObjectManager.instance.CheckCPlane()) return;
+            
+			ObjectManager.instance.gameObject.GetComponent<Point3DProcessor>().AddPoint();
+		}
+	}
+}
